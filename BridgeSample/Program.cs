351dcb9b9d92009ce94f08930927483029384ea3
USER <USER@DESKTOP-20DRNS1>
2022-12-29 20:43:22 +0330

    Last Update

--- BridgeSample/Program.cs
+++ BridgeSample/Program.cs
@@ -1,8 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using BridgeSample.MailService;
 
-Console.WriteLine("Hello, World!");
-
+ 
 RefinedMailService mailService = new RefinedMailService();
 mailService.Send(new EmailInforMationDto {
 Message="message",
@@ -11,21 +10,6 @@ Title = "title"
 });
 
 Console.ReadKey();
-
-Console.ReadKey();
-
-Console.ReadKey();
-
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
-Console.WriteLine("Hello, World!");
-
-
-Console.ReadKey();
-Console.WriteLine("Hello, World!");
-
-Console.WriteLine("Hello, World!");
+ 
 
 
